a702df676f9d5decb3908438d044b8fd732dea40
Askar <TherediusYT@gmail.com>
2023-11-25 20:29:25 +0600

    changing the UI and adding the Role field into the event model

--- calendarapp/models/event_member.py
+++ calendarapp/models/event_member.py
@@ -11,9 +11,10 @@ class EventMember(EventAbstract):
     user = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="event_members"
     )
+    role = models.CharField(max_length=100)
 
     class Meta:
-        unique_together = ["event", "user"]
+        unique_together = ["event", "user", "role"]
 
     def __str__(self):
-        return str(self.user)
+        return f"{self.user} - {self.role}"
